c45adcb146c1c35b80a03aaacfdd267b4ffa992f
Fubuking <gary19920103@hotmail.com.tw>
2020-12-01 17:57:58 +0800

    包裝Command

--- src/Main.kt
+++ src/Main.kt
@@ -27,60 +27,61 @@ fun main(args: Array<String>) {
         }
         return@run HashMap<String, String>()
     }
-    bot.addInterest(TdApi.UpdateNewMessage.CONSTRUCTOR) {
-        val newMessage = it as TdApi.UpdateNewMessage
-        if (newMessage.message.content.constructor == TdApi.MessageText.CONSTRUCTOR) {
-            val msgText = newMessage.message.content as TdApi.MessageText
-            if (matchGoodNight.containsMatchIn(msgText.text.text)) {
-                if (newMessage.message.sender.constructor == TdApi.MessageSenderUser.CONSTRUCTOR) {
-                    val senderId = (newMessage.message.sender as TdApi.MessageSenderUser).userId
-                    val lastDate = list[senderId]
-                    val nowDate = Date(System.currentTimeMillis()).date
-                    val chatId = newMessage.message.chatId
-                    val msgId = newMessage.message.id
-                    bot.client.send(TdApi.GetUser(senderId)) { usr ->
-                        val user = usr as TdApi.User
-                        var userName = user.firstName + user.lastName
-                        nameMap[senderId.toString()]?.let {
-                            userName = nameMap[senderId.toString()]!!
-                        }
-                        if (lastDate == nowDate) {
-                            val content: TdApi.InputMessageContent = TdApi.InputMessageText(TdApi.FormattedText("請快去睡覺", null), false, true)
-                            bot.client.send(TdApi.SendMessage(chatId, 0, msgId, null, null, content)) {
-                            }
-                        } else {
-                            val content: TdApi.InputMessageContent = TdApi.InputMessageText(TdApi.FormattedText("願明天對$userName" + "來說也是美好的一天", null), false, true)
-                            bot.client.send(TdApi.SendMessage(chatId, 0, msgId, null, null, content)) {
-                            }
-                            list[senderId] = nowDate
-                        }
-                    }
-                }
-            }
-        }
-    }
     // 註冊bot指令
-    bot.addCommandListener(object : CommandListener {
-        override fun onCommand(chatId: Long, senderId: Int, msgId: Long, command: String, arg: String) {
-            if (command == "/myname") {
-                var content: TdApi.InputMessageContent
-                if (arg.isEmpty()) {
-                    content = TdApi.InputMessageText(TdApi.FormattedText("不明白", null), false, true)
-                } else {
-                    nameMap[senderId.toString()] = arg
-                    content = TdApi.InputMessageText(TdApi.FormattedText("OK", null), false, true)
-                    bot.client.send(TdApi.SendMessage(chatId, 0, msgId, null, null, content), null)
-                    val f = File("user.json")
-                    if (!f.exists()) {
-                        f.createNewFile()
+    with(bot) {
+        addInterest(TdApi.UpdateNewMessage.CONSTRUCTOR) {
+            val newMessage = it as TdApi.UpdateNewMessage
+            if (newMessage.message.content.constructor == TdApi.MessageText.CONSTRUCTOR) {
+                val msgText = newMessage.message.content as TdApi.MessageText
+                if (matchGoodNight.containsMatchIn(msgText.text.text)) {
+                    if (newMessage.message.sender.constructor == TdApi.MessageSenderUser.CONSTRUCTOR) {
+                        val senderId = (newMessage.message.sender as TdApi.MessageSenderUser).userId
+                        val lastDate = list[senderId]
+                        val nowDate = Date(System.currentTimeMillis()).date
+                        val chatId = newMessage.message.chatId
+                        val msgId = newMessage.message.id
+                        client.send(TdApi.GetUser(senderId)) { usr ->
+                            val user = usr as TdApi.User
+                            var userName = user.firstName + user.lastName
+                            nameMap[senderId.toString()]?.let {
+                                userName = nameMap[senderId.toString()]!!
+                            }
+                            var content: TdApi.InputMessageContent
+                            if (lastDate == nowDate) {
+                                content = TdApi.InputMessageText(TdApi.FormattedText("請快去睡覺", null), false, true)
+                            } else {
+                                content = TdApi.InputMessageText(TdApi.FormattedText("願明天對$userName" + "來說也是美好的一天", null), false, true)
+                                list[senderId] = nowDate
+                            }
+                            client.send(TdApi.SendMessage(chatId, 0, msgId, null, null, content), null)
+                        }
                     }
-                    f.writeText(gson.toJson(nameMap), Charset.forName("utf-8"))
                 }
-                bot.client.send(TdApi.SendMessage(chatId, 0, msgId, null, null, content), null)
             }
         }
-    })
-    bot.start()
+        // 註冊bot指令
+        addCommandListener(object : CommandListener {
+            override fun onCommand(chatId: Long, senderId: Int, msgId: Long, command: String, arg: String) {
+                if (command == "/myname") {
+                    var content: TdApi.InputMessageContent
+                    if (arg.isEmpty()) {
+                        content = TdApi.InputMessageText(TdApi.FormattedText("不明白", null), false, true)
+                    } else {
+                        nameMap[senderId.toString()] = arg
+                        content = TdApi.InputMessageText(TdApi.FormattedText("OK", null), false, true)
+                        client.send(TdApi.SendMessage(chatId, 0, msgId, null, null, content), null)
+                        val f = File("user.json")
+                        if (!f.exists()) {
+                            f.createNewFile()
+                        }
+                        f.writeText(gson.toJson(nameMap), Charset.forName("utf-8"))
+                    }
+                    client.send(TdApi.SendMessage(chatId, 0, msgId, null, null, content), null)
+                }
+            }
+        })
+        start()
+    }
     while (!bot.isClosed()) {
         Thread.sleep(1000)
     }
