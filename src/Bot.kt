a443a20cd2fe6b89b817db31b337343d6eb93c92
Fubuking <gary19920103@hotmail.com.tw>
2020-12-02 12:39:30 +0800

    包裝Command

--- src/Bot.kt
+++ src/Bot.kt
@@ -4,7 +4,6 @@ import org.drinkless.tdlib.TdApi.*
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
-import java.util.concurrent.locks.Lock
 import java.util.concurrent.locks.ReentrantLock
 
 /**
